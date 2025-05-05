35cc42850b8aff2e0c5a28b7651eab08f75fe683
AlvinN04 <alvin.nguyen3@education.nsw.gov.au>
2025-05-06 09:16:23 +1000

    Update copilot.py

--- Year 10 Game/copilot.py
+++ Year 10 Game/copilot.py
@@ -112,6 +112,17 @@ def playing_state():
     if keys[pygame.K_DOWN]:
         sprite_rect.y += sprite_speed
 
+    # Camera boundaries to prevent moving out of the level
+    if camera_x > 0:
+        camera_x = 0
+    if camera_x < -level_width + width:
+        camera_x = -level_width + width
+
+    if camera_y > 0:
+        camera_y = 0
+    if camera_y < -level_height + height:
+        camera_y = -level_height + height
+
     # Draw player and enemy
     screen.blit(sprite_image, sprite_rect)
     screen.blit(penguin_image, penguin_rect)
